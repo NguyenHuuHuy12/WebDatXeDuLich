850bf4ff5f2aa3a4e3d81461904448d11076e516
NguyenHuuHuy12 <23130136@st.hcmuaf.edu.vn>
2025-10-25 23:46:50 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+// Thêm hiệu ứng nhỏ cho nút bấm
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.style.transform = "scale(0.95)";
+        setTimeout(() => (btn.style.transform = "scale(1)"), 150);
+    });
+});
