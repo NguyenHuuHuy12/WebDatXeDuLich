2a87507fc3a3a335022b38318eb74538dcaa56ba
NguyenHuuHuy12 <23130136@st.hcmuaf.edu.vn>
2025-11-25 23:36:02 +0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,7 +1,30 @@
-document.getElementById("signUp").addEventListener("click", () => {
-    document.getElementById("container").classList.add("right-panel-active");
-});
+const btnLogin = document.getElementById("btnLogin");
+const btnRegister = document.getElementById("btnRegister");
 
-document.getElementById("signIn").addEventListener("click", () => {
-    document.getElementById("container").classList.remove("right-panel-active");
-});
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+
+const switchToRegister = document.getElementById("switchToRegister");
+const switchToLogin = document.getElementById("switchToLogin");
+
+// Hàm đổi tab
+function showLogin() {
+    btnLogin.classList.add("active");
+    btnRegister.classList.remove("active");
+    loginForm.classList.add("show");
+    registerForm.classList.remove("show");
+
+}
+
+function showRegister() {
+    btnRegister.classList.add("active");
+    btnLogin.classList.remove("active");
+    registerForm.classList.add("show");
+    loginForm.classList.remove("show");
+}
+
+btnLogin.onclick = showLogin;
+btnRegister.onclick = showRegister;
+
+switchToRegister.onclick = showRegister;
+switchToLogin.onclick = showLogin;
