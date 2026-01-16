9bdd5b3f7d9cbe939a7349bae8db22ed6f290c05
NguyenTranXuanDao-23130044 <23130044@st.hcmuaf.edu.vn>
2026-01-17 01:29:53 +0700

    Add files via upload

---
+++ Notification.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.sql.Timestamp;
+
+
+public class Notification {
+    private int notificationId;
+    private Integer userId;
+    private String title;
+    private String message;
+    private String type;
+    private boolean isRead;
+    private Timestamp createdAt;
+
+    public Notification() {}
+
+    // Getters and Setters
+    public int getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(int notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
