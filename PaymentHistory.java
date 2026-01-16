2a28e82780a947fbe260d194a977c331a72b2ca8
NguyenTranXuanDao-23130044 <23130044@st.hcmuaf.edu.vn>
2026-01-17 01:25:50 +0700

    Add files via upload

---
+++ PaymentHistory.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.sql.Timestamp;
+
+
+public class PaymentHistory {
+    private int paymentId;
+    private int bookingId;
+    private double amount;
+    private String paymentMethod;
+    private String transactionId;
+    private String paymentStatus;
+    private Timestamp paymentDate;
+
+    public PaymentHistory() {}
+
+    // Getters and Setters
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Timestamp getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Timestamp paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
