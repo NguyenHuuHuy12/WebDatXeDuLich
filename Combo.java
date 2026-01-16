21ddf566286907ac65e0c8a96065d86743025b4e
NguyenTranXuanDao-23130044 <23130044@st.hcmuaf.edu.vn>
2026-01-17 01:23:21 +0700

    Add files via upload

---
+++ Combo.java
@@ -0,0 +1,110 @@
+package model;
+
+import model.CarType;
+
+import java.sql.Timestamp;
+public class Combo {
+    private int comboId;
+    private String name;
+    private String description;
+    private int numberOfPeople;
+    private Integer carTypeId;
+    private double price;
+    private String imageUrl;
+    private String benefits;
+    private String status;
+    private Timestamp createdAt;
+
+    private CarType carType;
+
+    public Combo() {}
+
+    // Getters and Setters
+    public int getComboId() {
+        return comboId;
+    }
+
+    public void setComboId(int comboId) {
+        this.comboId = comboId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public Integer getCarTypeId() {
+        return carTypeId;
+    }
+
+    public void setCarTypeId(Integer carTypeId) {
+        this.carTypeId = carTypeId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+}
